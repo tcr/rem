f6c7ddbefd3c6d4fa153980aa32fcf050f50d46c
Tim Cameron Ryan <id@timryan.org>
2012-08-31 22:43:18 -0400

    Include builtin schemas

---
+++ builtin/tumblr.json
@@ -0,0 +1,10 @@
+{
+  "2": {
+    "name": "Tumblr",
+    "docs": "http://www.tumblr.com/docs/en/api/v2",
+    "control": "http://www.tumblr.com/oauth/apps",
+
+    "base": "http://api.tumblr.com/v2",
+    "configParams": {"api_key": "key"}
+  }
+}
--- src/remutil.js
+++ src/remutil.js
@@ -205,7 +205,7 @@ remutil.lookup = function (name) {
   var fs = require('fs');
   var path = require('path');
   try {
-    return JSON.parse(fs.readFileSync(path.join(__dirname, '../common', name + '.json')));
+    return JSON.parse(fs.readFileSync(path.join(__dirname, '../builtin', name + '.json')));
   } catch (e) {
     return null;
   }

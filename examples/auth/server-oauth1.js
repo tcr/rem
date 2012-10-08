22da035a80d9ec302820111f34b031c520373f4b
Tim Cameron Ryan <id@timryan.org>
2012-10-08 12:25:06 -0400

    Clarify server oauth

--- examples/auth/server-oauth1.js
+++ examples/auth/server-oauth1.js
@@ -1,9 +1,10 @@
 // npm install rem read express
 var fs = require('fs');
-var rem = require('../../rem');
+var rem = require('rem');
 var read = require('read');
 var express = require('express');
 
+// Create the application. Authentication relies on having session capability.
 var app = express();
 app.use(express.cookieParser());
 app.use(express.session({
@@ -12,35 +13,38 @@ app.use(express.session({
 app.listen(3000);
 
 // Create the Dropbox OAuth API.
-
 var dbox = rem.load('dropbox', 1.0).prompt()
 var oauth = rem.oauth(dbox, "http://localhost:3000/oauth/callback/");
 
-app.use(oauth.middleware(function(req, res, next) {
-  console.log("Authenticated user.");
-  return res.redirect('/');
+// The oauth middleware intercepts the callback url that we set when we
+// created the oauth middleware.
+app.use(oauth.middleware(function (req, res, next) {
+  console.log("User is now authenticated.");
+  res.redirect('/');
 }));
 
-app.get('/login/', function(req, res) {
-  return oauth.startSession(req, function(url) {
-    return res.redirect(url);
+// Login URL calls oauth.startSession, which redirects to an oauth URL.
+app.get('/login/', function (req, res) {
+  oauth.startSession(req, function (url) {
+    res.redirect(url);
   });
 });
 
+// When the user is logged in, the "req.user" variable is set. This is
+// an authenticated api you can use to make REST calls.
 app.get('/', function(req, res) {
   if (!req.user) {
     res.write("<h1>Unauthenticated.</h1>");
-    return res.end("<a href='/login/'>Log in with OAuth</a>");
+    res.end("<a href='/login/'>Log in with OAuth</a>");
   } else {
     res.write('<h1>Authenticated.</h1>');
-    return req.user('metadata/sandbox/').get(function(err, json) {
-      var f, _i, _len, _ref;
+    req.user('metadata/sandbox/').get(function(err, json) {
       res.write('<pre>');
       res.write('Sandbox contents: (error ' + err + ')\n');
       res.write(json.contents.map(function (file) {
         return ' - ' + file.path;
       }).join('\n'));
-      return res.end();
+      res.end();
     });
   }
 });

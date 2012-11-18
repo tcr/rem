cbe35d9cae3233aaf5bb6b507421ce921e0dc6ac
Tim Cameron Ryan <id@timryan.org>
2012-11-17 20:30:06 -0500

    Revert USER_AGENT

--- src/rem.js
+++ src/rem.js
@@ -16,7 +16,7 @@ var remutil = typeof require == 'undefined' ? remutil : require('./remutil');
 rem.util = remutil;
 
 // Configuration.
-rem.USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11';
+rem.USER_AGENT = 'Mozilla/5.0 (compatible; REMbot/1.0; +http://remlib.org/)';
 rem.CONFIG_FILE = null;
 
 /**

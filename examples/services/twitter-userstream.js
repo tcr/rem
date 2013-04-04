115b34cb7f9b3a5010bf1cbb25274be2c0c665e2
evansimpson <esimpson.rescue@gmail.com>
2013-04-04 14:40:32 -0400

    Updating twitter-userstream.js. Still using clarinet

--- examples/services/twitter-userstream.js
+++ examples/services/twitter-userstream.js
@@ -1,16 +1,33 @@
 // npm install rem read clarinet
 var rem = require('rem');
 var read = require('read');
-var carrier = require('carrier');
+//var carrier = require('carrier');
+var clarinet = require('clarinet');
 
 // Create Twitter API, prompting for key/secret.
 // NOTE: Requires Twitter API 1.1
 var tw = rem.connect('twitter.com', 1.1).prompt(function (err, user){
+
   // Pass the statuses/sample stream to a JSON parser and print only the tweets.
+  user.stream('user').get(function (err, stream) {
+    stream.pipe(clarinet.createStream()).on('key', function (key) {
+      if (key == 'text') {
+        this.once('value', function (tweet) {
+          console.log(String(tweet));
+        })
+      }
+    });
+  });
+
+  // Same stream implemented with carrier. 
+  // Exits with "SyntaxError: Unexpected end of input"
+  /*
   user.stream('user').get(function (err, stream) {
     carrier.carry(stream, function(line){
       var line = JSON.parse(line);
       console.log(line);
     });
   });
+  */
 });
+

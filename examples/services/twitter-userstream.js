a3e59510e1b7087781184eff3dfd7278183e488e
evansimpson <esimpson.rescue@gmail.com>
2013-04-04 14:31:30 -0400

    fixed twitter-stream.js

--- examples/services/twitter-userstream.js
+++ examples/services/twitter-userstream.js
@@ -1,23 +1,16 @@
 // npm install rem read clarinet
 var rem = require('rem');
 var read = require('read');
-var clarinet = require('clarinet');
+var carrier = require('carrier');
 
 // Create Twitter API, prompting for key/secret.
 // NOTE: Requires Twitter API 1.1
-var tw = rem.connect('twitter.com', 1.1).prompt();
-
-// Authenticate user via the console.
-rem.console(tw, function (err, user) {
-
+var tw = rem.connect('twitter.com', 1.1).prompt(function (err, user){
   // Pass the statuses/sample stream to a JSON parser and print only the tweets.
   user.stream('user').get(function (err, stream) {
-    stream.pipe(clarinet.createStream()).on('key', function (key) {
-      if (key == 'text') {
-        this.once('value', function (tweet) {
-          console.log(String(tweet));
-        })
-      }
+    carrier.carry(stream, function(line){
+      var line = JSON.parse(line);
+      console.log(line);
     });
   });
 });
